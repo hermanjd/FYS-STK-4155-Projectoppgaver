8834c74a7772b48c297b68350f63c7a767e7ab06
Harithelamin <harithelamin@gmail.com>
2022-09-24 16:02:43 +0200

    Frank Function
    Last update with Frank Function

---
+++ Project1/Code/FF.py
@@ -0,0 +1,53 @@
+from array import array
+from pyclbr import Function
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+from random import random, seed
+from traitlets import Any
+
+#Frank Function
+class FF:
+    def __init__(self, Array1, Array2):
+      self.Array1 = Array1
+      self.Array1 = Array2
+      #Print a message
+
+      print('Method has been called.')
+      fig = plt.figure()
+      ax = fig.gca(projection='3d')
+
+      x = np.array(Array1)
+      y = np.array(Array2)
+      x, y = np.meshgrid(x,y)
+
+      def FrankeFunction(x,y):
+        term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+        term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+        term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+        term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+        return term1 + term2 + term3 + term4
+
+
+      z = FrankeFunction(x, y)
+
+      # Plot the surface.
+      surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                        linewidth=0, antialiased=False)
+
+      # Customize the z axis.
+      ax.set_zlim(-0.10, 1.40)
+      ax.zaxis.set_major_locator(LinearLocator(10))
+      ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+      # Add a color bar which maps values to colors.
+      fig.colorbar(surf, shrink=0.5, aspect=5)
+
+      plt.show()
+
+
+    
+
+   
