1fb8bcf43f315648d27e54fc82b8ec12ac9a6f8f
hermanjd <herman.dragland@gmail.com>
2022-10-04 20:17:27 +0200

    Adding train-test data and testing multiple polynomials

--- Project1/Code/oppgave_b.py
+++ Project1/Code/oppgave_b.py
@@ -2,13 +2,18 @@ import numpy as np
 from matplotlib import cm
 import numpy as np
 import StatFunctions
+from sklearn.model_selection import train_test_split
 
-N = 1000000
+N = 4000
 x = np.random.uniform(0, 1, N)
 y = np.random.uniform(0, 1, N)
-z = StatFunctions.FrankeFunction(x,y)
-X = StatFunctions.create_X(x,y,5)
-B = StatFunctions.findBetaValues(X,z)
-Y = StatFunctions.findY(X,B)
-mse = StatFunctions.evaluateRSquared(z,Y)
-print(mse)
+z = StatFunctions.FrankeFunctionWithNoise(x,y,0.02)
+
+for i in range(0, 6):
+	X = StatFunctions.create_X(x,y,i)
+	X_train, X_test, z_train, z_test = train_test_split(X,z,test_size=0.2)
+	B = StatFunctions.findBetaValues(X_train,z_train)
+	Y = StatFunctions.findY(X_test,B)
+	mse = StatFunctions.evaluateMSE(z_test,Y)
+	RSquared = StatFunctions.evaluateRSquared(z_test,Y)
+	print("Plynomial degree: {} Mean square error: {} RSquared: {}".format(i,mse,RSquared))
