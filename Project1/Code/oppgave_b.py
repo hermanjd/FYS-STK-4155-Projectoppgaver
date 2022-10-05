b5f0a03691c8387eabb58734654f456c0c903287
hermanjd <herman.dragland@gmail.com>
2022-10-05 11:43:03 +0200

    Adding train test

--- Project1/Code/oppgave_b.py
+++ Project1/Code/oppgave_b.py
@@ -4,16 +4,20 @@ import numpy as np
 import StatFunctions
 from sklearn.model_selection import train_test_split
 
-N = 4000
+N = 50
 x = np.random.uniform(0, 1, N)
 y = np.random.uniform(0, 1, N)
-z = StatFunctions.FrankeFunctionWithNoise(x,y,0.02)
+z = StatFunctions.FrankeFunctionWithNoise(x,y,0.1) #adding some noise to the data
 
 for i in range(0, 6):
 	X = StatFunctions.create_X(x,y,i)
 	X_train, X_test, z_train, z_test = train_test_split(X,z,test_size=0.2)
 	B = StatFunctions.findBetaValues(X_train,z_train)
+	YTrain = StatFunctions.findY(X_train,B)
 	Y = StatFunctions.findY(X_test,B)
+	mseTrain = StatFunctions.evaluateMSE(z_train,YTrain)
 	mse = StatFunctions.evaluateMSE(z_test,Y)
+	RSquaredTrain = StatFunctions.evaluateRSquared(z_train,YTrain)
 	RSquared = StatFunctions.evaluateRSquared(z_test,Y)
-	print("Plynomial degree: {} Mean square error: {} RSquared: {}".format(i,mse,RSquared))
+	print("TRAIN Polynomial degree: {} Mean square error: {} RSquared: {}".format(i,mseTrain,RSquaredTrain))
+	print("TEST- Polynomial degree: {} Mean square error: {} RSquared: {}".format(i,mse,RSquared))
