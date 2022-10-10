1c7d47aaef4aa22d93a0ed0c827cdd1a56735c48
Herman Johan Dragland <herman.johan.dragland@innovasjonnorge.no>
2022-10-10 10:07:24 +0200

    Refactoring

--- Project1/Code/oppgave_c.py
+++ Project1/Code/oppgave_c.py
@@ -2,8 +2,6 @@ import numpy as np
 from matplotlib import cm
 import numpy as np
 import StatFunctions
-from sklearn.model_selection import train_test_split
-from sklearn.utils import resample
 import matplotlib.pyplot as plt
 
 N = 65
@@ -11,7 +9,7 @@ polynomialDegrees = 5
 x = np.random.uniform(0, 1, N)
 y = np.random.uniform(0, 1, N)
 z = StatFunctions.FrankeFunctionWithNoise(x,y,0.0) #adding some noise to the data
-polydegree, error, bias, variance =StatFunctions.PolynomialOLSBootstrapRegression(x,y,z,0.2,polynomialDegrees,N)
+polydegree, error, bias, variance = StatFunctions.PolynomialOLSBootstrapRegression(x,y,z,0.2,polynomialDegrees,N)
 plt.plot(polydegree, error, label='Error')
 plt.plot(polydegree, bias, label='bias')
 plt.plot(polydegree, variance, label='Variance')
