ec8ee54155386a65002413640ad25cb19b1daaf3
Harithelamin <harithelamin@gmail.com>
2022-10-11 12:22:53 +0200

    Update oppgave-e.py

--- Project1/Code/oppgave-e.py
+++ Project1/Code/oppgave-e.py
@@ -1,69 +1,37 @@
-
-import matplotlib.pyplot as plt
+#Ridge Regression on the Franke function
 import numpy as np
-from sklearn import linear_model
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import PolynomialFeatures
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-from scipy import misc
 import StatFunctions
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 
 
-def FindBeta(x, y, degree, alpha):
-    X = np.c_[x,y]
-    poly = PolynomialFeatures(degree)
-    X_ = poly.fit_transform(X)
-    ridge = linear_model.RidgeCV(alphas=np.array([alpha]))
-    ridge.fit(X_, z)
-    return ridge.coef_
 
-def RidgeRegression(x, y, z, degree=5, alpha=10**(-6), verbose=False):
-    # Split data
-    x_train = np.random.rand(100,1)
-    y_train = np.random.rand(100,1)
-    z = StatFunctions.FrankeFunction(x_train,y_train)
-    beta = FindBeta(x, y, degree, alpha)
+def Ridge(x, y, z, polynomialDegrees, l=0.1):
+    # Calculate matrix with x, y - polynomials
+    M_ = np.c_[x, y]
+    poly = PolynomialFeatures(polynomialDegrees)
+    M = poly.fit_transform(M_)
 
-    # show pilot
-    poly = PolynomialFeatures(degree)
-    x_, y_ = np.meshgrid(x, y)
-    x = x_.reshape(-1,1)
-    y = y_.reshape(-1,1)
-    M = np.c_[x, y]
-    M_ = poly.fit_transform(M)
-    predict = M_.dot(beta.T)
+    # Calculate beta
+    A = np.arange(1, polynomialDegrees + 2)
+    rows = np.sum(A)
+    beta = (np.linalg.inv(M.T.dot(M) + l * np.identity(rows))).dot(M.T).dot(z)
 
-    if verbose:
-        print ("X_: ", np.shape(alpha))
-        print ("M: ", np.shape(M))
-        print ("M_: ", np.shape(M_))
-        print ("predict: ", np.shape(predict))
-    
-    # show pilot
-    poly = PolynomialFeatures(degree)
-    x_, y_ = np.meshgrid(x, y)
-    x = x_.reshape(-1,1)
-    y = y_.reshape(-1,1)
-    M = np.c_[x, y]
-    M_ = poly.fit_transform(M)
-    predict = M_.dot(beta.T)
     return beta
 
-
 if __name__ == '__main__':
-    x = np.arange(0, 1, 0.05).reshape((20,1))
-    y = np.arange(0, 1, 0.05).reshape((20,1))
-
-    z = StatFunctions.FrankeFunction(x,y)
-    beta = RidgeRegression(x,y,z,5,5**(-3),True)
-    mse = StatFunctions.evaluateMSE(x,y)
-    print(mse)
-    print (beta)
-
-    beta_list = beta.ravel().tolist()
-    print ("ok")
-
-    
+        N = 100
+        polynomialDegrees = 5
+        x = np.random.uniform(0, 1, N)
+        y = np.random.uniform(0, 1, N)
+        z = StatFunctions.FrankeFunctionWithNoise(x,y,0.0) #adding some noise to the data
+        polydegree_bootstrap, error_bootstrap, bias_bootstrap, variance_bootstrap = StatFunctions.PolynomialOLSBootstrapResampling(x,y,z,0.2,polynomialDegrees,N)
+        polydegree_crossValidation, error_crossValidation = StatFunctions.PolynomialOLSCrossValidation(x,y,z,polynomialDegrees,20)
+        Ridge= Ridge(x, y, z, polynomialDegrees, N)
+        plt.plot(polydegree_bootstrap, error_crossValidation, label='Crossvalidation')
+        plt.plot(polydegree_bootstrap, error_bootstrap, label='Bootstrap')
+        plt.plot(Ridge, label='LassoR egression')        
+        plt.legend()
+        plt.show()
 
